72179c680d184b5256ba974f753f11a0bb5a83e4
zjoney <zdjoey@126.com>
2020-02-21 13:37:32 +0800

    config

---
+++ src/build/webpack.dev.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+
+module.exports = merge(commonConfig, {
+    
+})
+
