07f2c37e8f69136636d8dc4ff5a6175c40ab0032
zjoney <zdjoey@126.com>
2020-02-22 22:03:24 +0800

    add pages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
 import './styles/main.less';
-import App from './App';
+import AppRouter from './routers/AppRouter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+moment.locale('zh-cn');
 
+const App = () => (
+  <ConfigProvider locale={zhCN}>
+    <AppRouter />
+  </ConfigProvider>
+);
+
+ReactDom.render(<App />, document.getElementById('app'));
+
+// 热更新
+if (module.hot) {
+  module.hot.accept(err => {
+    if (err) {
+      console.error('module.hot，', err);
+    }
+  });
+}
