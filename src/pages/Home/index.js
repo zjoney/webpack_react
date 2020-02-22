0e41fbebb7e20c550c3fc8becb02ae1ed4e4e087
zjoney <zdjoey@126.com>
2020-02-22 22:16:20 +0800

    login

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import './style.less';
+
+const FormDemoPage = () => {
+
+
+  return (
+    <div className="page-form-demo page-content">
+     Home demo
+    </div>
+  );
+};
+
+// export default observer(FormDemoPage);
+export default FormDemoPage;
