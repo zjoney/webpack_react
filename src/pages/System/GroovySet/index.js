b71f42d8cc2e5eff0e3de474cc98c8ffeeeb48af
zjoney <zdjoey@126.com>
2020-02-22 22:21:46 +0800

    system

---
+++ src/pages/System/GroovySet/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GrovvPage = () => {
+
+  return (
+    <div className="page-form-demo page-content">
+     GrovvPage demo
+    </div>
+  );
+};
+
+export default GrovvPage;
---
+++ src/pages/System/Star/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StartPage = () => {
+
+  return (
+    <div className="page-form-demo page-content">
+     StartPage demo
+    </div>
+  );
+};
+
+export default StartPage;
