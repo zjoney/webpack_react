3869ad4d6ddd8e6698a1cf38f76b8dd18a8fb49b
zjoney <zdjoey@126.com>
2020-02-22 22:23:22 +0800

    system  user

---
+++ src/pages/System/User/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserPage = () => {
+
+  return (
+    <div className="page-form-demo page-content">
+     UserPage demo
+    </div>
+  );
+};
+
+export default UserPage;
