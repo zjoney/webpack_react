0b9d1539d80ea8a0bb3b6234d8fdb099eef59884
zjoney <zdjoey@126.com>
2020-02-22 22:24:38 +0800

    Welcome

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomePage = () => {
+
+  return (
+    <div className="page-form-demo page-content">
+     WelcomePage demo
+    </div>
+  );
+};
+
+export default WelcomePage;
