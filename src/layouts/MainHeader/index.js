e7e250ca799da2bf24b8a43c5d11609512c728d4
zjoney <zdjoey@126.com>
2020-02-25 11:17:01 +0800

    code

--- src/layouts/MainHeader/index.js
+++ src/layouts/MainHeader/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react';
 
 import { appStores } from '@/stores';
 import './style.less';
-import { Layout, Row, Col, Icon } from 'antd';
+import { Layout, Row, Col, Icon, Dropdown, Menu } from 'antd';
 
+const menu = () => (
+ <Menu>
+     <Menu.Item key="0">
+        <Icon type="smile" />
+         个人信息
+     </Menu.Item>
+     <Menu.Divider />
+     <Menu.Item KEY="1">
+         <Link to="/login">
+             <Icon type="logout" />
+             退出登录
+         </Link>
+     </Menu.Item>
+ </Menu>
+);
 
 const MainHeader = () => {
     console.log('')
@@ -19,6 +35,14 @@ const MainHeader = () => {
                     onClick={globalStore.toggleCollapsed}
 
                   />
+               </Col>
+               <Col>
+                <Dropdown overlay={menu} trigger={['click', 'hover']} placement="bottomCenter">
+                    <div className="user-info">
+                       <span className="user-img" />
+    <span className="user-info">{globalStore.userInfo.loginName}</span>
+                    </div>
+                </Dropdown>
                </Col>
             </Row>
         </Layout.Header>
