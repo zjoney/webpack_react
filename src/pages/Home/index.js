70882e0c720b364e766f34bd5c7bea1839788fdd
zjoney <zdjoey@126.com>
2020-02-25 18:09:46 +0800

    home components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 
 import Store from './store';
 import './style.less';
 
 const HomePage = () => {
+  const pageStore = useContext(Store);
+  const [num, setNum] = useState(0);
 
+  useEffect(() => {
+    pageStore.qryTableData();
+  }, [])
   return (
-    <div className="page-home">
-     Home demo
+    <div className="page-home page-content">
+      <h2>{pageStore.pageTitle}</h2>
+      <div>
+        <span>num值：{num}</span>
+        <Button
+          type="primary"
+          size="small"
+          style={{marginLeft: 10 }}
+          onClick={() => setNum(num +1 )}
+        >
+          add
+        </Button>
+      </div>
+      Home demo
     </div>
   );
 };
