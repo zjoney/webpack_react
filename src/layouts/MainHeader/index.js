e5a307bd5b01f9e792df7106a1c48a07eb3e9cab
zjoney <zdjoey@126.com>
2020-02-25 11:07:18 +0800

    layout header

--- src/layouts/MainHeader/index.js
+++ src/layouts/MainHeader/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { observer } from 'mobx-react';
+
+import { appStores } from '@/stores';
+import './style.less';
+import { Layout, Row, Col, Icon } from 'antd';
+
 
 const MainHeader = () => {
     console.log('')
+    const { globalStore } = appStores();
     return (
-        <div>MainHeader</div>
+        <Layout.Header className="main-header">
+            <Row type="flex" style={{paddingRight: 20}}>
+               <Col style={{flex: 1}}>
+                  <Icon 
+                    className="trigger"
+                    type={globalStore.collapsed ? 'menu-unfold':'meu-fold'}
+                    onClick={globalStore.toggleCollapsed}
+
+                  />
+               </Col>
+            </Row>
+        </Layout.Header>
     )
 }
 
