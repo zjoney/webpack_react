a921540b3b079e816d289acf50770db99418dc60
zjoney <zdjoey@126.com>
2020-02-25 17:24:49 +0800

    utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,25 +2,24 @@
 *date: 2020-02-25 17:16
 *
 */
-const isDev = process.env.NODE_ENV === 'development'; 
+const isDev = process.env.NODE_ENV === 'development';
 
 export function autoMatch(prefix) {
-  // eslint-disable-next-line no-unused-vars
   let baseUrl = '';
-
-  if(isDev) {
-// 开发环境配置，通过proxy转发请求
-baseUrl = `/${prefix || 'testapi' }`;
+  if (isDev) {
+    // 开发环境配置，通过proxy转发请求
+    baseUrl = `/${prefix || 'testapi'}`;
   } else {
-// 生产环境配置 
-switch (prefix) {
-  case 'baidu': 
-     baseUrl = window.location.baidu;
-     break;
-  default:
-    baseUrl = window.location.default;
-}
+    // 生产环境配置 
+    // switch (prefix) {
+    //   case 'baidu':
+    //     baseUrl = window.location.baidu;
+    //     break;
+    //   default:
+    //     baseUrl = window.location.default;
+    // }
   }
+  return baseUrl;
 }
 
 export function checkStatus(params) {
